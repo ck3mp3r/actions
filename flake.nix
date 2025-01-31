09fc244acdd45d7fa3377e76d2759c23d93549f9
Christian Kemper <christian.kemper@me.com>
2025-01-31 21:56:50 +0000

    * Update Dependabot to run weekly and update Flake.lock with new `nixpkgs` revision.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "virtual environments";
 
-  inputs.nixpkgs.url = "github:NixOs/nixpkgs/23.11";
+  inputs.nixpkgs.url = "github:NixOs/nixpkgs/release-24.11";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
-    self,
     flake-utils,
     devshell,
     nixpkgs,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
